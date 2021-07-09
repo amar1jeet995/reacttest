6a6ce0c31596afcd991a6d55e01b378df47b4cc8
AmarjeetK <75114144+amar1jeet995@users.noreply.github.com>
2021-07-09 17:17:02 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> Built The pipeline at Last.
+          Edit <code>src/App.js</code> Built it.
         </p>
         <a
           className="App-link"
